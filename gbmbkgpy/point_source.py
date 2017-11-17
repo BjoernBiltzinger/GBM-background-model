6290a93b38a5abbf48052d73b6ccaa826c76d10c
J. Michael Burgess <jmichaelburgess@gmail.com>
2017-11-17 14:14:45 +0100

    ready for point sources class

---
+++ gbmbkgpy/point_source.py
@@ -0,0 +1,41 @@
+import numpy as np
+
+from gbmbkgpy.utils.external_prop import ExternalProps
+from gbmbkgpy.work_module_refactor import calculate
+
+
+class PointSource(object):
+
+    def __init__(self, name, ra, dec):
+
+        pass
+
+
+    def set_relative_location(self, time, coordinate):
+
+        # get the separation
+
+        #interpolate
+
+
+
+        pass
+
+
+
+    def separation_angle(self, met):
+
+        pass
+
+
+
+    @property
+    def location(self):
+
+        return
+
+
+    @property
+    def name(self):
+
+        return
