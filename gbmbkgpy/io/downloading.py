76f480f8bd42a38c88ac9b3271eed18e62363510
J. Michael Burgess <jmichaelburgess@gmail.com>
2017-11-19 22:34:41 +0100

    added DL functions

---
+++ gbmbkgpy/io/downloading.py
@@ -0,0 +1,86 @@
+import os
+import urllib2
+
+from gbmbkgpy.io.package_data import get_path_of_data_dir
+
+
+def download_flares(year):
+    """This function downloads a yearly solar flar data file and stores it in the appropriate folder\n
+    Input:\n
+    download.flares ( year = YYYY )\n"""
+
+    # create the appropriate folder if it doesn't already exist and switch to it
+    file_path = os.path.join(get_path_of_data_dir(), 'flares/')
+    if not os.access(file_path, os.F_OK):
+        print("Making New Directory")
+        os.mkdir(file_path)
+
+    os.chdir(file_path)
+    if year == 2016:
+        url = (
+        'ftp://ftp.ngdc.noaa.gov/STP/space-weather/solar-data/solar-features/solar-flares/x-rays/goes/xrs/goes-xrs-report_' + str(
+            year) + 'ytd.txt')
+    else:
+        url = (
+        'ftp://ftp.ngdc.noaa.gov/STP/space-weather/solar-data/solar-features/solar-flares/x-rays/goes/xrs/goes-xrs-report_' + str(
+            year) + '.txt')
+    file_name = '%s.dat' % str(year)
+
+    u = urllib2.urlopen(url)
+
+
+    with open(file_name, 'wb') as f:
+        meta = u.info()
+        file_size = int(meta.getheaders("Content-Length")[0])
+        print "Downloading: %s Bytes: %s" % (file_name, file_size)
+
+        file_size_dl = 0
+        block_sz = 8192
+        while True:
+            buffer = u.read(block_sz)
+            if not buffer:
+                break
+
+            file_size_dl += len(buffer)
+            f.write(buffer)
+            status = r"%10d  [%3.2f%%]" % (file_size_dl, file_size_dl * 100. / file_size)
+            status = status + chr(8) * (len(status) + 1)
+   #         print status,
+
+
+
+def download_lat_spacecraft(week):
+    """This function downloads a weekly lat-data file and stores it in the appropriate folder\n
+    Input:\n
+    download.lat_spacecraft ( week = XXX )\n"""
+
+    # create the appropriate folder if it doesn't already exist and switch to it
+    file_path = os.path.join(get_path_of_data_dir(), 'lat/')
+    if not os.access(file_path, os.F_OK):
+        print("Making New Directory")
+        os.mkdir(file_path)
+
+    #os.chdir(file_path)
+
+    url = 'http://heasarc.gsfc.nasa.gov/FTP/fermi/data/lat/weekly/spacecraft/lat_spacecraft_weekly_w%d_p202_v001.fits' % week
+    file_name = os.path.join(file_path,url.split('/')[-1])
+    u = urllib2.urlopen(url)
+    f = open(file_name, 'wb')
+    meta = u.info()
+    file_size = int(meta.getheaders("Content-Length")[0])
+    print "Downloading: %s Bytes: %s" % (file_name, file_size)
+
+    file_size_dl = 0
+    block_sz = 8192
+    while True:
+        buffer = u.read(block_sz)
+        if not buffer:
+            break
+
+        file_size_dl += len(buffer)
+        f.write(buffer)
+        #status = r"%10d  [%3.2f%%]" % (file_size_dl, file_size_dl * 100. / file_size)
+        #status = status + chr(8) * (len(status) + 1)
+        #print status,
+
+    f.close()
