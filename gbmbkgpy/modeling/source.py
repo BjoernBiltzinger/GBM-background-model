1c10d65e11ebd046f20b1dbb54bceef578da61a3
Kunzweiler, Felix <felix.kunzweiler@scandio.de>
2017-12-11 15:30:10 +0100

    Add SAA Source

--- gbmbkgpy/modeling/source.py
+++ gbmbkgpy/modeling/source.py
@@ -1,4 +1,4 @@
-FLARE_SOURCE, CONTINUUM_SOURCE, POINT_SOURCE = 'flare_source', 'continuum_source', 'point_source'
+FLARE_SOURCE, CONTINUUM_SOURCE, POINT_SOURCE, SAA_SOURCE = 'flare_source', 'continuum_source', 'point_source', 'saa_source'
 from scipy import integrate
 
 class Source(object):
@@ -7,7 +7,7 @@ class Source(object):
         self._source_type = source_type
         self._shape = shape
 
-        assert source_type in [POINT_SOURCE, CONTINUUM_SOURCE, FLARE_SOURCE], 'improper source'
+        assert source_type in [POINT_SOURCE, CONTINUUM_SOURCE, FLARE_SOURCE, SAA_SOURCE], 'improper source'
 
     def __call__(self, x):
 
@@ -35,7 +35,6 @@ class ContinuumSource(Source):
         super(ContinuumSource, self).__init__(name, CONTINUUM_SOURCE, continuum_shape)
 
 
-
 class FlareSource(Source):
     def __init__(self, name, flare_shape):
         super(FlareSource, self).__init__(name, FLARE_SOURCE, flare_shape)
@@ -44,5 +43,11 @@ class FlareSource(Source):
 class PointSource(Source):
     def __init__(self, name, point_shape):
         super(PointSource, self).__init__(name, POINT_SOURCE, point_shape)
+
+
+class SAASource(Source):
+    def __init__(self, name, saa_shape):
+        super(SAASource, self).__init__(name, SAA_SOURCE, saa_shape)
+
 
 
