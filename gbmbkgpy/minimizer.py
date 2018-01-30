e7b443834db8689f8a801f689c8692b028621c7f
J. Michael Burgess <jmichaelburgess@gmail.com>
2018-01-30 10:23:14 +0100

    begin mini class

--- gbmbkgpy/minimizer.py
+++ gbmbkgpy/minimizer.py
@@ -1,14 +1,47 @@
 import numpy as np
 import scipy.optimize as optimize
-
+import pandas as pd
 
 
 def Minimizer(self, likelihood):
 
 
-    self._likelihood
+    self._likelihood = likelihood
 
 
-def fit(self):
+    def fit(self, n_interations = 100):
 
-    optimize.minimize(self._likelihood)
+        # first do the linear fit for normalizations
+
+        #optimize.minimize(self._likelihood)
+
+        # now free all the needed parameters
+        # and fit for them from the current starting positions
+
+
+
+        # save the fit results and errors
+
+        self._save()
+
+
+
+        # display the results
+
+        self.display()
+
+    def _save(self):
+
+        pass
+
+
+    def display(self):
+        """
+        display the results using pandas series or dataframe
+        
+        
+        :param self: 
+        :return: 
+        """
+
+        pass
