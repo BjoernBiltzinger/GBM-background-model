c52e0c31ede819b012fbf2c8a8fa079ea75d94f6
Kunzweiler, Felix <felix.kunzweiler@tum.de>
2018-02-01 13:22:59 +0100

    Implement Minimizer Class

--- gbmbkgpy/minimizer.py
+++ gbmbkgpy/minimizer.py
@@ -1,34 +1,83 @@
 import numpy as np
-import scipy.optimize as optimize
+from scipy.optimize import minimize
 import pandas as pd
+from datetime import datetime
 
 
-def Minimizer(self, likelihood):
+class Minimizer(object):
+
+    def __init__(self, likelihood):
+
+        self._likelihood = likelihood
+        self._result_steps = {}
+
+    def fit(self, n_interations = 6):
+
+        start = datetime.now()
+
+        # First do the linear fit for normalizations and fix the other parameters
+        self._likelihood.fix_parameters(self._likelihood.get_not_normalization_parameter_list)
+
+        step = datetime.now()
+        start_params = self._likelihood.get_free_parameter_values
+        bounds = self._likelihood.get_free_parameter_bounds
+        self._result_steps['1'] = minimize(self._likelihood, start_params, method='L-BFGS-B', bounds=bounds, options={'maxiter': 10000, 'gtol': 1e-08})
+        print "1. The linear optimization took: {}".format(datetime.now() - step)
 
 
-    self._likelihood = likelihood
+        # Fix the normalizations and fit for the other parameters
+        self._likelihood.fix_parameters(self._likelihood.get_normalization_parameter_list[0:4])
+        self._likelihood.unfix_parameters(self._likelihood.get_not_normalization_parameter_list)
+
+        step = datetime.now()
+        start_params = self._likelihood.get_free_parameter_values
+        bounds = self._likelihood.get_free_parameter_bounds
+        self._result_steps['2'] = minimize(self._likelihood, start_params, method='L-BFGS-B', bounds=bounds, options={'maxiter': 10000, 'gtol': 1e-08})
+        print "2. The SAA optimization took: {}".format(datetime.now() - step)
 
 
-    def fit(self, n_interations = 100):
+        # Unfix all parameters and fit all with bounds
+        self._likelihood.unfix_parameters(self._likelihood.get_normalization_parameter_list)
 
-        # first do the linear fit for normalizations
+        step = datetime.now()
+        start_params = self._likelihood.get_free_parameter_values
+        bounds = self._likelihood.get_free_parameter_bounds
+        self._result_steps['3'] = minimize(self._likelihood, start_params, method='L-BFGS-B', bounds=bounds, options={'maxiter': 10000, 'gtol': 1e-08})
+        print "3. The full constrained optimization took: {}".format(datetime.now() - step)
 
-        #optimize.minimize(self._likelihood)
+        if n_interations > 3:
+            # Fit all parameters without bounds in three runs to improve speed and accuracy
+            step = datetime.now()
+            start_params = self._likelihood.get_free_parameter_values
+            self._result_steps['4'] = minimize(self._likelihood, start_params, method='Powell', options={})
+            print "4. The 1st unconstrained optimization took: {}".format(datetime.now() - step)
 
-        # now free all the needed parameters
-        # and fit for them from the current starting positions
+        if n_interations > 4:
+            step = datetime.now()
+            start_params = self._likelihood.get_free_parameter_values
+            self._result_steps['5'] = minimize(self._likelihood, start_params, method='Powell', options={})
+            print "5. The 2nd unconstrained optimization took: {}".format(datetime.now() - step)
 
+        if n_interations > 5:
+            #Final run with improved accuracy
+            step = datetime.now()
+            start_params = self._likelihood.get_free_parameter_values
+            self._result_steps['6'] = minimize(self._likelihood, start_params, method='Powell', options={'xtol': 0.000001, 'ftol': 0.000001})
+            print "6. The 3rd unconstrained optimization took: {}".format(datetime.now() - step)
+            self.result = self._result_steps['6']
+        print "The total Optimization took: {}".format(datetime.now() - start)
 
-
+        if n_interations < 6:
+            self.result = self._result_steps['%s' % n_interations]
         # save the fit results and errors
 
         self._save()
 
-
-
         # display the results
 
         self.display()
+
+        return self.result
 
     def _save(self):
 
@@ -44,4 +93,11 @@ def Minimizer(self, likelihood):
         :return: 
         """
 
-        pass
+        data_dic = {}
+
+        for i, parameter in enumerate(self._likelihood._parameters.itervalues()):
+                    data_dic[parameter.name] = {}
+                    data_dic[parameter.name]['fitted value'] = parameter.value
+        fittet_params = pd.DataFrame.from_dict(data_dic, orient='index')
+
+
