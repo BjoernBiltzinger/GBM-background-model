c9a29e25efa188adead7bfa6199eacce64dc2d9b
Kunzweiler, Felix <felix.kunzweiler@tum.de>
2018-02-07 14:05:19 +0100

    Cleanup of minimizer class

--- gbmbkgpy/minimizer/minimizer.py
+++ gbmbkgpy/minimizer/minimizer.py
@@ -17,9 +17,8 @@ class Minimizer(object):
         1. Fit linear/normalization paramters
         2. Fit SAA parameters
         3. Fit all parameters with bounds
-        4. Fit all parameters without bounds: Powell
-        5. Fit all parameters without bounds: Powell
-        6. Fit all parameters without bounds and high precision: Powell
+        4. Fit all parameters without bounds multiple times: Powell
+        5. Fit all parameters without bounds and high precision: Powell
         :param n_interations:
         :return:
         """
@@ -32,58 +31,34 @@ class Minimizer(object):
 
         # First do the linear fit for normalizations and fix the other parameters
         self._likelihood.fix_parameters(self._likelihood.get_not_normalization_parameter_list)
-
-        step = datetime.now()
-        start_params = self._likelihood.get_free_parameter_values
-        bounds = self._likelihood.get_free_parameter_bounds
-        self._result_steps['1'] = minimize(self._likelihood, start_params, method=method_1, bounds=bounds, options={'maxiter': 10000, 'gtol': 1e-08, 'ftol':1e-10})
-        print "1. The linear optimization took: {}".format(datetime.now() - step)
-
+        self._fit_with_bounds(method_1, type="linear", iter_nr=1)
 
         # Fix the normalizations and fit for the other parameters
         self._likelihood.fix_parameters(self._likelihood.get_normalization_parameter_list[0:4])
         self._likelihood.unfix_parameters(self._likelihood.get_not_normalization_parameter_list)
-
-        step = datetime.now()
-        start_params = self._likelihood.get_free_parameter_values
-        bounds = self._likelihood.get_free_parameter_bounds
-        self._result_steps['2'] = minimize(self._likelihood, start_params, method=method_1, bounds=bounds, options={'maxiter': 10000, 'gtol': 1e-08, 'ftol':1e-10})
-        print "2. The SAA optimization took: {}".format(datetime.now() - step)
-
+        self._fit_with_bounds(method_1, type="SAA", iter_nr=2)
 
         # Unfix all parameters and fit all with bounds
         self._likelihood.unfix_parameters(self._likelihood.get_normalization_parameter_list)
+        self._fit_with_bounds(method_1, type="full constrained", iter_nr=3)
 
-        step = datetime.now()
-        start_params = self._likelihood.get_free_parameter_values
-        bounds = self._likelihood.get_free_parameter_bounds
-        self._result_steps['3'] = minimize(self._likelihood, start_params, method=method_1, bounds=bounds, options={'maxiter': 10000, 'gtol': 1e-08, 'ftol':1e-10})
-        print "3. The full constrained optimization took: {}".format(datetime.now() - step)
-
+        # Fit all parameters without bounds in three runs to improve speed and accuracy
         if n_interations > 3:
-            # Fit all parameters without bounds in three runs to improve speed and accuracy
-            step = datetime.now()
-            start_params = self._likelihood.get_free_parameter_values
-            self._result_steps['4'] = minimize(self._likelihood, start_params, method='Powell', options={})
-            print "4. The 1st unconstrained optimization took: {}".format(datetime.now() - step)
+            for i in range(4, n_interations-1):
+                self._fit_without_bounds(method_2, iter_nr=i, options={})
 
-        if n_interations > 4:
-            step = datetime.now()
-            start_params = self._likelihood.get_free_parameter_values
-            self._result_steps['5'] = minimize(self._likelihood, start_params, method='Powell', options={})
-            print "5. The 2nd unconstrained optimization took: {}".format(datetime.now() - step)
+            # Final run with improved accuracy
+            self._fit_without_bounds(method_2, iter_nr=n_interations, options={'xtol': 0.000001, 'ftol': 0.000001})
 
-        if n_interations > 5:
-            #Final run with improved accuracy
-            step = datetime.now()
-            start_params = self._likelihood.get_free_parameter_values
-            self._result_steps['6'] = minimize(self._likelihood, start_params, method='Powell', options={'xtol': 0.000001, 'ftol': 0.000001})
-            print "6. The 3rd unconstrained optimization took: {}".format(datetime.now() - step)
-            self.result = self._result_steps['6']
+        self.result = self._result_steps['%s' % n_interations]
+
+
         print "The total Optimization took: {}".format(datetime.now() - start)
 
-        if n_interations < 6:
-            self.result = self._result_steps['%s' % n_interations]
+        print "The Optimization ended with status:  {}".format(self.result.status)
+
+        print "Success = {}".format(self.result.success)
+
         # save the fit results and errors
 
         self._save()
@@ -93,6 +68,21 @@ class Minimizer(object):
         self.display()
 
         return self.result
+
+    def _fit_with_bounds(self, method, type, iter_nr):
+
+        step = datetime.now()
+        start_params = self._likelihood.get_free_parameter_values
+        bounds = self._likelihood.get_free_parameter_bounds
+        self._result_steps[str(iter_nr)] = minimize(self._likelihood, start_params, method=method, bounds=bounds,
+                                           options={'maxiter': 10000, 'gtol': 1e-08, 'ftol': 1e-10})
+        print "{}. The {} optimization took: {}".format(str(iter_nr), type, datetime.now() - step)
+
+    def _fit_without_bounds(self, method, iter_nr, options):
+        step = datetime.now()
+        start_params = self._likelihood.get_free_parameter_values
+        self._result_steps[str(iter_nr)] = minimize(self._likelihood, start_params, method=method, options=options)
+        print "{}. The {}st unconstrained optimization took: {}".format(iter_nr, iter_nr - 3, datetime.now() - step)
 
     def _save(self):
 
