1434811dc8906a72e1661b0795371cf64b48c193
Kunzweiler, Felix <felix.kunzweiler@tum.de>
2018-02-08 16:18:12 +0100

    Add download data file to downloading

--- gbmbkgpy/io/downloading.py
+++ gbmbkgpy/io/downloading.py
@@ -1,4 +1,5 @@
 import os
+import shutil
 import urllib2
 
 from gbmbkgpy.io.package_data import get_path_of_data_dir
@@ -28,30 +29,8 @@ def download_flares(year):
 
     path_to_file = download_file(url)
 
-    os.rename(path_to_file, file_path + file_name)
+    shutil.move(path_to_file, file_path + file_name)
 
-    """
-    u = urllib2.urlopen(url)
-
-
-    with open(file_name, 'wb') as f:
-        meta = u.info()
-        file_size = int(meta.getheaders("Content-Length")[0])
-        print "Downloading: %s Bytes: %s" % (file_name, file_size)
-
-        file_size_dl = 0
-        block_sz = 8192
-        while True:
-            buffer = u.read(block_sz)
-            if not buffer:
-                break
-
-            file_size_dl += len(buffer)
-            f.write(buffer)
-            status = r"%10d  [%3.2f%%]" % (file_size_dl, file_size_dl * 100. / file_size)
-            status = status + chr(8) * (len(status) + 1)
-   #         print status,
-    """
 
 
 def download_lat_spacecraft(week):
@@ -73,29 +52,26 @@ def download_lat_spacecraft(week):
 
     file_name = 'lat_spacecraft_weekly_w%d_p202_v001.fits' % week
 
-    os.rename(path_to_file, file_path + file_name)
+    shutil.move(path_to_file, file_path + file_name)
 
+def download_data_file(date, type, detector):
+    # create the appropriate folder if it doesn't already exist and switch to it
 
-    """
-    file_name = os.path.join(file_path,url.split('/')[-1])
-    u = urllib2.urlopen(url)
-    f = open(file_name, 'wb')
-    meta = u.info()
-    file_size = int(meta.getheaders("Content-Length")[0])
-    print "Downloading: %s Bytes: %s" % (file_name, file_size)
+    year = '20%s' % date[:2]
+    month = date[2:-2]
+    day = date[-2:]
 
-    file_size_dl = 0
-    block_sz = 8192
-    while True:
-        buffer = u.read(block_sz)
-        if not buffer:
-            break
+    file_path = os.path.join(get_path_of_data_dir(), type, '/', date)
+    if not os.access(file_path, os.F_OK):
+        print("Making New Directory")
+        os.mkdir(file_path)
 
-        file_size_dl += len(buffer)
-        f.write(buffer)
-        #status = r"%10d  [%3.2f%%]" % (file_size_dl, file_size_dl * 100. / file_size)
-        #status = status + chr(8) * (len(status) + 1)
-        #print status,
+    # os.chdir(file_path)
 
-    f.close()
-    """
+    url = 'https://heasarc.gsfc.nasa.gov/FTP/fermi/data/gbm/daily/{}/{}/{}/current/glg_spechist_{}_{}_v00.fit'.format(year, month, day, detector, date)
+
+    path_to_file = download_file(url)
+
+    file_name = 'glg_spechist_n{}_{}_v00.fit'.format(year, month, day, detector, date)
+
+    shutil.move(path_to_file, file_path + file_name)
