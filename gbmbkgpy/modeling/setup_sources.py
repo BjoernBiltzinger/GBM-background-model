c36f1c2a0a7a190f9feeee0add3e6d064cfd63df
BjoernBiltzinger <bjoernbiltzinger@gmx.de>
2018-07-12 16:06:00 +0200

    added a class_parameter use_SAA to continous data.
    If use_SAA is set to FALSE the SAA phases + 5000 seconds after each one are not included in the data and the fit for the SAA phases is not done

--- gbmbkgpy/modeling/setup_sources.py
+++ gbmbkgpy/modeling/setup_sources.py
@@ -34,16 +34,17 @@ def setup_sources(cd, ep, echan, include_point_sources=False):
 
     # SAA Decay Source
     SAA_Decay_list = []
-    saa_n = 0
-    day_start = np.array(cd._day_met)
-    start_times = np.append(day_start, cd.saa_mean_times)
+    if cd.use_SAA():
+        saa_n = 0
+        day_start = np.array(cd._day_met)
+        start_times = np.append(day_start, cd.saa_mean_times)
 
-    for time in start_times:
-        saa_dec = SAA_Decay(str(saa_n))
-        saa_dec.set_saa_exit_time(np.array([time]))
-        saa_dec.set_time_bins(cd.time_bins[2:-2])
-        SAA_Decay_list.append(SAASource('saa_' + str(saa_n), saa_dec))
-        saa_n += 1
+        for time in start_times:
+            saa_dec = SAA_Decay(str(saa_n))
+            saa_dec.set_saa_exit_time(np.array([time]))
+            saa_dec.set_time_bins(cd.time_bins[2:-2])
+            SAA_Decay_list.append(SAASource('saa_' + str(saa_n), saa_dec))
+            saa_n += 1
 
     # Solar Continuum Source
     sol_con = Solar_Continuum()
