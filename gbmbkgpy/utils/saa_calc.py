1f27167df812f1f7af1c5b12dd43fe53ff4e83a2
Biltzinger Bjoern <bbiltzing@necromancer.mpe.mpg.de>
2019-07-22 11:43:49 +0200

    Added SAA_calc class, small corrections to geometry_calc_class

--- gbmbkgpy/utils/saa_calc.py
+++ gbmbkgpy/utils/saa_calc.py
@@ -1,1 +1,113 @@
-test
+import numpy as np
+
+
+class SAA_calc(object):
+    def __init__(self, time_bins, bins_to_add=8, use_SAA=True, time_after_SAA=5000):
+        """
+        Initalize the SAA calculation that calculates the start and stop times of the SAAs and builds
+        masks. 
+        :params time_bins: the time bins of the day
+        :params bins_to_add: number of bins to add before and after the SAA always
+        :params use_SAA: bool flag that indicates if the SAA is included in the model
+        :params time_after_SAA: time after the SAA to ignore if the SAA is not included in the model
+        """
+        
+        assert type(bins_to_add)==int, 'bins_to_add gives the number of time_bins to add before and after the SAA. It must therefore be an int but it is {}.'.format(type(bins_to_add))
+        assert type(time_bins)==np.ndarray, 'Invalid type for time_bins. Must be an array but is {}.'.format(type(time_bins))
+        assert type(use_SAA)==bool, 'use_SAA must be a bool but is {}'.format(use_SAA)
+        assert type(time_after_SAA)==int, 'time_after_SAA must be a int but is a {}'.format(type(time_after_SAA))
+        
+        
+
+        self._time_bins = time_bins
+        self._build_masks(bins_to_add, use_SAA)
+
+    @property
+    def saa_mask(self):
+        return self._saa_mask
+        
+    def _build_masks(self, bins_to_add, use_SAA):
+        """
+        Calculates masks that cover the SAAs and some time bins before and after the SAAs
+        :params bins_to_add: number of bins to add to mask before and after time bin
+        """
+
+        # Calculate the time jump between two successive time bins. During the SAAs no data is recorded.
+        # This leads to a time jump between two successive time bins before and after the SAA.
+        jump = self._time_bins[1:,0]-self._time_bins[:-1,1]
+
+        # Get mask for which the jump is > 1 second
+        jump_large = jump>1
+        
+        # Get the indices of the time bins for which the jump to the previous time bin is >1 second
+        # +1 is needed because we started with second time bin (we can not calculate the time jump
+        # between the first time bin and the time bin before that one)
+        idx = jump_large.nonzero()[0] + 1
+
+        # Build slices, that have as first entry start of SAA and as second end of SAA
+        slice_idx = np.array(slice_disjoint(idx))
+
+        # Add bins_to_add before and after SAAs
+        slice_idx[:, 0][np.where(slice_idx[:, 0] >= bins_to_add)] =\
+            slice_idx[:, 0][np.where(slice_idx[:, 0] >= bins_to_add)] - bins_to_add
+
+        slice_idx[:, 1][np.where(slice_idx[:, 1] <= len(self._time_bins) - 1 - bins_to_add)] =\
+            slice_idx[:, 1][np.where(slice_idx[:, 1] <= len(self._time_bins) - 1 - bins_to_add)] + bins_to_add
+        
+        # Find the times of the exits
+        if slice_idx[-1 , 1] == len(self._time_bins) - 1:
+            # the last exit is just the end of the array
+            saa_exit_idx = slice_idx[:-1, 1]
+        else:
+            saa_exit_idx = slice_idx[:, 1]
+
+        self._saa_exit_time_bins = self._time_bins[saa_exit_idx]
+
+        # make a saa mask from the slices:
+        self._saa_mask = np.ones(len(self._time_bins))
+        for sl in slice_idx:
+            self._saa_mask[sl[0]:sl[1]+1]=False
+
+        # If the use_SAA flag is False we add 5000 seconds after every SAA to the mask
+        # For these times the SAAs can hava a large influence and should therefore be ignored
+        # if the SAAs is not modeled
+        if not use_SAA:
+            # Set first time_after_SAA seconds False
+            j = 0
+            while time_after_saa > self._time_bins[j,1] - self._time_bins[0,0]:
+                j += 1
+            self._saa_mask[0:j] = False
+
+            # Do the same for every SAA exit. We have to be carefull tonot cause an error when the time
+            # after a SAA is less than 500 seconds
+            for i in range(len(slice_idx)):
+                
+                if self._time_bins[:,0][slice_idx[i, 1]] + time_after_saa > self._time_bins[-1,0]:
+                    self._saa_mask[slice_idx[i, 1]:len(self._time_bins)] = False
+                else:
+                    j = 0
+                    while self._time_bins[:,0][slice_idx[i, 1]] + time_after_saa > self._time_bins[:,0][slice_idx[-1, 1] + j]:
+                        j += 1
+                    self._saa_mask[slice_idx[i, 1]:slice_idx[i, 1] + j] = False
+        
+
+    def slice_disjoint(self, arr):
+        """
+        Returns an array of disjoint indices from a bool array
+        :param arr: and array of bools
+        """
+
+        slices = []
+        start_slice = arr[0]
+        counter = 0
+        for i in range(len(arr) - 1):
+            if arr[i + 1] > arr[i] + 1:
+                end_slice = arr[i]
+                slices.append([start_slice, end_slice])
+                start_slice = arr[i + 1]
+                counter += 1
+        if counter == 0:
+            return [[arr[0], arr[-1]]]
+        if end_slice != arr[-1]:
+            slices.append([start_slice, arr[-1]])
+        return slices
