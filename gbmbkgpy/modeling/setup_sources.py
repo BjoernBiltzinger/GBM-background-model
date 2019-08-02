127f2e70dd231aaaa68531ddaf4358117078b214
Björn Biltzinger <bjoernbiltzinger@gmx.de>
2019-08-02 17:06:05 +0200

    Clean up setup sources class, is now very general and allows a lot of input

--- gbmbkgpy/modeling/setup_sources.py
+++ gbmbkgpy/modeling/setup_sources.py
@@ -1,12 +1,12 @@
 
-from gbmbkgpy.modeling.source import Source, ContinuumSource, FlareSource, PointSource, SAASource, GlobalSource, FitSpectrumSource
-from gbmbkgpy.modeling.function import Function, ContinuumFunction
-from gbmbkgpy.modeling.functions import (Solar_Flare, Solar_Continuum, SAA_Decay,
-                                         Magnetic_Continuum, Cosmic_Gamma_Ray_Background, Point_Source_Continuum, Earth_Albedo_Continuum, offset, Magnetic_Secondary_Continuum,West_Effect_Continuum, Magnetic_Continuum_Global, Magnetic_Constant_Global, Earth_Albedo_Continuum_Fit_Spectrum, Cosmic_Gamma_Ray_Background_Fit_Spectrum,SAA_Decay_Linear)
+from gbmbkgpy.modeling.source import ContinuumSource, SAASource, GlobalSource, FitSpectrumSource
 
-from gbmbkgpy.modeling.model_cr_asymmetrie import cr_asymmetrie_nb_n5
+from gbmbkgpy.modeling.functions import (SAA_Decay,Magnetic_Continuum, Cosmic_Gamma_Ray_Background,
+                                         Point_Source_Continuum, Earth_Albedo_Continuum, offset,
+                                         Earth_Albedo_Continuum_Fit_Spectrum, Cosmic_Gamma_Ray_Background_Fit_Spectrum)
 
 import numpy as np
+import scipy.interpolate as interpolate
 
 
     # see if we have mpi and/or are upalsing parallel                                                                                                                                                                                                                          
@@ -26,7 +26,76 @@ try:
 except:
 
     using_mpi = False
-def setup_SAA(cd ,saa_object, echan):
+
+def Setup(cd, saa_object, ep, geom_object,echan_list=[],response_object=None, albedo_cgb_object=None, use_SAA=False,
+          use_CR=True, use_Earth=True, use_CGB=True, use_all_ps=False, point_source_list=[], fix_Earth=False,
+          fix_CGB=False):
+    """
+    Setup all sources
+    :param cd: continous_data object
+    :param saa_object: saa precaculation object
+    :param ep: external prob object
+    :param geom_object: geometry precalculation object
+    :param echan_list: list of all echans which should be used
+    :param response_object: response precalculation object
+    :param albedo_cgb_object: albedo_cgb precalculation object
+    :param use_SAA: use saa?
+    :param use_CR: use cr?
+    :param use_Earth: use earth?
+    :param use_CGB: use cgb?
+    :param use_all_ps: use all ps?
+    :param point_source_list: PS to use
+    :param fix_Earth: fix earth spectrum?
+    :param fix_CGB: fix cgb spectrum?
+    :return:
+    """
+
+    assert len(echan_list)>1, 'Please give at least one echan'
+
+    assert max(echan_list)<len(cd.ebins) and min(echan_list)>0, 'Please onyl use valid ebin numbers ' \
+                                                                'between 0 and {}'.format(len(cd.ebins))
+    assert type(use_SAA)==bool and type(use_CR)==bool and type(use_Earth)==bool and type(use_CGB)==bool and \
+           type(fix_Earth)==bool and type(fix_CGB)==bool and type(use_all_ps)==bool, 'Please only use True or False here.'
+
+    total_sources = []
+
+    # Go through all possible types of sources and add them in a list
+
+    for echan in echan_list:
+
+        if use_SAA:
+            total_sources.append(setup_SAA(cd, echan))
+
+        if use_CR:
+            total_sources.append(setup_CosmicRays(cd, ep, saa_object, echan))
+
+    if use_all_ps:
+        total_sources.append(setup_ps(cd, ep, saa_object, response_object, geom_object, include_point_sources=True))
+
+    elif len(point_source_list)!=0:
+        total_sources.append(setup_ps(cd, ep, saa_object, response_object, geom_object,
+                                      point_source_list=point_source_list))
+
+    if use_Earth:
+
+        if fix_Earth:
+            total_sources.append(setup_earth_fix(cd, albedo_cgb_object, saa_object))
+
+        else:
+            total_sources.append(setup_earth_free(cd, albedo_cgb_object, saa_object))
+
+    if use_CGB:
+
+        if fix_CGB:
+            total_sources.append(setup_cgb_fix(cd, albedo_cgb_object, saa_object))
+
+        else:
+            total_sources.append(setup_cgb_free(cd, albedo_cgb_object, saa_object))
+
+    return total_sources
+
+
+def setup_SAA(cd , echan):
     """
     Setup for SAA sources
     :param saa_object: SAA precalculation object
@@ -51,7 +120,8 @@ def setup_SAA(cd ,saa_object, echan):
         SAA_Decay_list.append(SAASource('saa_{:d} echan_{}'.format(saa_n,echan), saa_dec, echan))
         saa_n += 1
     return SAA_Decay_list
-def setup_CosmicRays(cd, ep, echan):
+
+def setup_CosmicRays(cd, ep, saa_object, echan):
     """
     Setup for CosmicRay source
     :param ep: external prob object
@@ -62,7 +132,7 @@ def setup_CosmicRays(cd, ep, echan):
     
     Constant = offset(str(echan))
     Constant.set_function_array(cd.cgb_background(cd.time_bins[2:-2]))
-    Constant.set_saa_zero(cd.saa_mask[2:-2])
+    Constant.set_saa_zero(saa_object.saa_mask[2:-2])
     # precalculate the integration over the time bins
     Constant.integrate_array(cd.time_bins[2:-2])
     Constant_Continuum = ContinuumSource('Constant_echan_{:d}'.format(echan), Constant, echan)
@@ -70,17 +140,18 @@ def setup_CosmicRays(cd, ep, echan):
     # Magnetic Continuum Source
     mag_con = Magnetic_Continuum(str(echan))
     mag_con.set_function_array(ep.mc_l((cd.time_bins[2:-2])))
-    mag_con.set_saa_zero(cd.saa_mask[2:-2])
+    mag_con.set_saa_zero(saa_object.saa_mask[2:-2])
     mag_con.remove_vertical_movement()
     # precalculate the integration over the time bins
     mag_con.integrate_array(cd.time_bins[2:-2])
     Source_Magnetic_Continuum = ContinuumSource('McIlwain L-parameter_echan_{:d}'.format(echan),
                                                 mag_con, echan)
+    return [Constant_Continuum,Source_Magnetic_Continuum]
 
     
-def setup_ps(cd, ep, include_point_sources=False, point_source_list=[]):
+def setup_ps(cd, ep, saa_object, response_object, geom_object, include_point_sources=False, point_source_list=[]):
     """
-    set up the global sources which are the same for all echans.
+    Set up the global sources which are the same for all echans.
     At the moment the Earth Albedo and the CGB.
     :param cd:
     :param echan:
@@ -89,119 +160,113 @@ def setup_ps(cd, ep, include_point_sources=False, point_source_list=[]):
     assert len(point_source_list)==0 or include_point_sources==False, 'Either include all point sources ' \
                                                                           'or give a list with the wanted sources.' \
                                                                           'Not both!'
-    # Earth Albedo Continuum Source
-    earth_albedo = Earth_Albedo_Continuum()
+    PS_Sources_list =[]
 
-    ###Testing rigidity dependence
-    #rigidity_array = (14.823/((ep.mc_l(cd.time_bins[2:-2]))**2))**(-1.13)
-    ##
-    
-    earth_albedo.set_function_array(cd.earth_rate_array(cd.time_bins[2:-2]))#*rigidity_array)
-    earth_albedo.set_saa_zero(cd.saa_mask[2:-2])
-    #earth_albedo.set_base_function_all_times(cd.point_base_rate(cd.time_bins[2:-2]))#
-    #earth_albedo.set_angle_of_points_all_times(cd.point_earth_angle(cd.time_bins[2:-2]))#
-    # precalculate the integration over the time bins
-    earth_albedo.integrate_array(cd.time_bins[2:-2])
-    Source_Earth_Albedo_Continuum = GlobalSource('Earth occultation', earth_albedo)
-
-    # Cosmic gamma-ray background
-    cgb = Cosmic_Gamma_Ray_Background()
-    cgb.set_function_array(cd.cgb_rate_array(cd.time_bins[2:-2]))
-    cgb.set_saa_zero(cd.saa_mask[2:-2])
-    cgb.integrate_array(cd.time_bins[2:-2])
-    Source_CGB_Continuum = GlobalSource('CGB', cgb)
-
-    
-    PS_Sources_list = []
     # Point-Source Sources
     if include_point_sources:
-        ep.build_point_sources(cd)
+        ep.build_point_sources(response_object, geom_object)
         PS_Continuum_dic = {}
-        PS_Sources_list = []
 
         for i, ps in enumerate(ep.point_sources.itervalues()):
             PS_Continuum_dic['{}'.format(ps.name)] = Point_Source_Continuum(str(i))
-            PS_Continuum_dic['{}'.format(ps.name)].set_function_array(ps.ps_rate_array(cd.time_bins[2:-2]))
-            PS_Continuum_dic['{}'.format(ps.name)].set_saa_zero(cd.saa_mask[2:-2])
+            rate_inter = interpolate.inter1d(ps.geometry_times, ps.ps_rate_array)
+            PS_Continuum_dic['{}'.format(ps.name)].set_function_array(rate_inter(cd.time_bins[2-2]))
+            PS_Continuum_dic['{}'.format(ps.name)].set_saa_zero(saa_object.saa_mask[2:-2])
             PS_Continuum_dic['{}'.format(ps.name)].integrate_array(cd.time_bins[2:-2])
 
             PS_Sources_list.append(GlobalSource('{}'.format(ps.name), PS_Continuum_dic['{}'.format(ps.name)]))
     if len(point_source_list)>0:
-        ep.build_some_source(cd, point_source_list)
+        ep.build_some_source(response_object, geom_object, point_source_list)
         PS_Continuum_dic = {}
-        PS_Sources_list = []
         for i, ps in enumerate(ep.point_sources.itervalues()):
             PS_Continuum_dic['{}'.format(ps.name)] = Point_Source_Continuum(str(i))
-            #PS_Continuum_dic[ps.name].set_function_array(cd.effective_angle(ps.calc_occ_array(cd.time_bins[2:-2]),
-            #                                                                echan))
             PS_Continuum_dic['{}'.format(ps.name)].set_function_array(ps.ps_rate_array(cd.time_bins[2:-2]))
-            PS_Continuum_dic['{}'.format(ps.name)].set_saa_zero(cd.saa_mask[2:-2])
+            PS_Continuum_dic['{}'.format(ps.name)].set_saa_zero(saa_object.saa_mask[2:-2])
+
             #precalculate the integration over the time bins
             PS_Continuum_dic['{}'.format(ps.name)].integrate_array(cd.time_bins[2:-2])
-            #PS_Continuum_dic[ps.name].set_earth_zero(ps.earth_occ_of_ps(cd.mean_time[2:-2]))
 
             PS_Sources_list.append(GlobalSource('{}'.format(ps.name), PS_Continuum_dic['{}'.format(ps.name)]))
 
-    ###############################################
-    # LAT ACD rate
-    #mag_con_lat = Magnetic_Continuum_Global()
-    #function_array = []
-    #basis = ep.lat_acd(cd.time_bins[2:-2], use_side = 'C')
-    #for i in range(8):
-    #    function_array.append(cd.ebins_size[i]*basis)
-    #mag_con_lat.set_function_array(np.array(function_array))
-    #mag_con_lat.set_saa_zero(cd.saa_mask[2:-2])
-    ##mag_con_lat.remove_vertical_movement()
-    # precalculate the integration over the time bins
-    #mag_con_lat.integrate_array(cd.time_bins[2:-2])
-    #Source_Magnetic_Continuum_LAT_ACD = GlobalSource('ACD rate', mag_con_lat)
+    return PS_Sources_list
 
-    # LAT ACD rate
-    #mag_con_lat = Magnetic_Constant_Global()
-    #function_array = []
-    #basis = np.ones_like(cd.time_bins[2:-2])
-    #for i in range(8):
-    #    function_array.append(cd.ebins_size[i]*basis)
-    #mag_con_lat.set_function_array(np.array(function_array))
-    #mag_con_lat.set_saa_zero(cd.saa_mask[2:-2])
-    #mag_con_lat.remove_vertical_movement()
-    # precalculate the integration over the time bins
-    #mag_con_lat.integrate_array(cd.time_bins[2:-2])
-    #Source_Magnetic_Constant = GlobalSource('CR constant', mag_con_lat)
 
-    
-    source_list = [Source_Earth_Albedo_Continuum, Source_CGB_Continuum] + PS_Sources_list
-    #source_list = [Source_Earth_Albedo_Continuum, Source_CGB_Continuum, Source_Magnetic_Continuum_LAT_ACD, Source_Magnetic_Constant] + PS_Sources_list
-    #source_list = PS_Sources_list
-    return source_list
-
-def setup_sources_fit_spectra(cd):
+def setup_earth_free(cd, albedo_cgb_object, saa_object):
     """
-    set up the global sources which are the same for all echans.
-    At the moment the Earth Albedo and the CGB.
+    Setup Earth Albedo source with free spectrum
     :param cd:
-    :param echan:
+    :param albedo_cgb_object:
+    :param saa_object:
     :return:
     """
 
-    # Earth Albedo Continuum Source
+    eff_response = albedo_cgb_object.earth_effective_response
+
     earth_albedo = Earth_Albedo_Continuum_Fit_Spectrum()
-    earth_albedo.set_response_array(cd.response_array_earth) #???
+    earth_albedo.set_response_array(eff_response)
     earth_albedo.set_basis_function_array(cd.time_bins[2:-2])
-    earth_albedo.set_saa_zero(cd.saa_mask[2:-2])
-    earth_albedo.set_interpolation_times(cd.interpolation_time)
-    earth_albedo.energy_boundaries(cd.Ebin_source)
+    earth_albedo.set_saa_zero(saa_object.saa_mask[2:-2])
+    earth_albedo.set_interpolation_times(albedo_cgb_object.geometry_times)
+    earth_albedo.energy_boundaries(albedo_cgb_object.Ebin_in_edge)
     Source_Earth_Albedo_Continuum = FitSpectrumSource('Earth occultation', earth_albedo)
 
-    # Cosmic gamma-ray background
+    return Source_Earth_Albedo_Continuum
+
+def setup_earth_fix(cd, albedo_cgb_object, saa_object):
+    """
+    Setup Earth Albedo source with fixed spectrum
+    :param cd:
+    :param albedo_cgb_object:
+    :param saa_object:
+    :return:
+    """
+
+    earth_albedo = Earth_Albedo_Continuum()
+    rate_inter = interpolate.inter1d(albedo_cgb_object.geometry_times, albedo_cgb_object.earth_rate_array)
+
+    earth_albedo.set_function_array(rate_inter(cd.time_bins[2:-2]))
+    earth_albedo.set_saa_zero(saa_object.saa_mask[2:-2])
+    earth_albedo.integrate_array(cd.time_bins[2:-2])
+    Source_Earth_Albedo_Continuum = GlobalSource('Earth Albedo', earth_albedo)
+
+    return Source_Earth_Albedo_Continuum
+
+
+def setup_cgb_free(cd, albedo_cgb_object, saa_object):
+    """
+    Setup CGB source with free spectrum
+    :param cd:
+    :param albedo_cgb_object:
+    :param saa_object:
+    :return:
+    """
+    eff_response = albedo_cgb_object.cgb_effective_response
+
     cgb = Cosmic_Gamma_Ray_Background_Fit_Spectrum()
-    
-    cgb.set_response_array(cd.response_array_cgb) #???                                                                                                                                                       
-    cgb.set_basis_function_array(cd.time_bins[2:-2])                                                                                                                                                     
-    cgb.set_saa_zero(cd.saa_mask[2:-2])
-    cgb.set_interpolation_times(cd.interpolation_time)
-    cgb.energy_boundaries(cd.Ebin_source)
-    Source_CGB_Continuum = FitSpectrumSource('CGB', cgb)
-    
-    source_list = [Source_Earth_Albedo_Continuum, Source_CGB_Continuum]
-    return source_list
+    cgb.set_response_array(eff_response)
+    cgb.set_basis_function_array(cd.time_bins[2:-2])
+    cgb.set_saa_zero(saa_object.saa_mask[2:-2])
+    cgb.set_interpolation_times(albedo_cgb_object.geometry_times)
+    cgb.energy_boundaries(albedo_cgb_object.Ebin_in_edge)
+    Source_CGB_Albedo_Continuum = FitSpectrumSource('CGB', cgb)
+
+    return Source_CGB_Albedo_Continuum
+
+
+def setup_cgb_fix(cd, albedo_cgb_object, saa_object):
+    """
+    Setup CGB source with fixed spectrum
+    :param cd:
+    :param albedo_cgb_object:
+    :param saa_object:
+    :return:
+    """
+    cgb = Cosmic_Gamma_Ray_Background()
+    rate_inter = interpolate.inter1d(albedo_cgb_object.geometry_times, albedo_cgb_object.cgb_rate_array)
+
+    cgb.set_function_array(rate_inter(cd.time_bins[2:-2]))
+    cgb.set_saa_zero(saa_object.saa_mask[2:-2])
+    cgb.integrate_array(cd.time_bins[2:-2])
+    Source_CGB_Albedo_Continuum = GlobalSource('Earth occultation', cgb)
+
+    return Source_CGB_Albedo_Continuum
