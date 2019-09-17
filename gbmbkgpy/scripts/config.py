756b553ecd6d9fc3f5084a375b2896c9ca1360d5
Björn Biltzinger <bjoernbiltzinger@gmx.de>
2019-09-17 09:57:42 +0200

    Write config file with all parameters that can be changed. The general_script does not need to be changed anymore for different setups.

---
+++ gbmbkgpy/scripts/config.py
@@ -0,0 +1,72 @@
+####################################################################################################
+################ Config file for all the input parameters ##########################################
+####################################################################################################
+
+
+
+################## General Input (Days, Detector, Data Type, Energy Channels) #######################
+
+general_dict = {'dates': ['190114', '190115'],
+               'detector': 'n7',
+               'data_type': 'cspec',
+               'echan_list': [5, 15, 45, 85, 100]}
+
+################# Input for response precalculation (how many grid poits?) ##########################
+
+response_dict = {'Ngrid': 40000}
+
+####### Input for SAA mask precaluclation (include SAA in model, time to exclude after SAA, ##########
+################################# delete short time intervals? ######################################
+
+saa_dict = {'use_SAA': False,
+            'time_after_SAA': 5000,
+            'short_time_intervals': False}
+
+###### Input for geometry calculation (n time bins per day to calculate the geometry ################
+
+geom_dict = {'n_bins_to_calculate': 800}
+
+##### Input for source Setup (use CR, use Earth, use CGB, point source list, fix earth, fix cgb #####
+
+setup_dict = {'use_CR': True,
+              'use_Earth': True,
+              'use_CGB': True,
+              'ps_list': ['CRAB'],
+              'fix_earth': False,
+              'fix_cgb': False}
+
+################################ Bounds for the different sources ###################################
+####### SAA: Amplitude and decay constant, CR: Constant and McIlwain normalization ##################
+####### Point source: Amplitude, Earth/CGB fixec: Amplitude, Earth/CGB free: Amplitude, #############
+############################ index1, index2 and break energy#########################################
+
+bounds_dict = {'saa_bound': [(1, 10**4), (10**-5, 10**-1)],
+               'cr_bound': [(0.1, 100), (0.1, 100)],
+               'earth_fixed_bound': [(0.001, 1)],
+               'cgb_fixed_bound': [(0.01, 0.5)],
+               'earth_free_bound': [(0.001, 1), (-8, -3), (1.1, 1.9), (20, 40)],
+               'cgb_free_bound': [(0.01, 0.5), (0.5, 1.7), (2.2, 3.1), (27, 40)],
+               'ps_bound': [(5, 100)]}
+
+##################### Input for multinest sampler ###################################################
+
+multi_dict = {'num_live_points': 400,
+              'constant_efficiency_mode': True}
+
+############################### Input for plotting ##################################################
+### bin_width to bin the data, change_time from MET to seconds since midnight, show residuals? ######
+### show data?, show grb trigger times? if yes at which time ('20:57:03.000' format) and what name? #
+### show ppc?, xlim and ylim (as tuple e.g. (0,100)), legend outside of the plot? ####################
+
+
+plot_dict = {'bin_width': 10,
+             'change_time': True,
+             'show_residuals': False,
+             'show_data': True,
+             'show_grb_trigger': True,
+             'times_mark': [],
+             'names_mark': [],
+             'ppc': True,
+             'xlim': None,
+             'ylim': None,
+             'legend_outside': False}
