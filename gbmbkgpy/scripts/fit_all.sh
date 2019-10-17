d31373b8424ee730d42fb52d2ddaa96aa2dbd3e9
Felix Kunzweiler <felix.kunzweiler@tum.de>
2019-10-17 20:18:35 +0200

    fix fit_all script

--- gbmbkgpy/scripts/fit_all.sh
+++ gbmbkgpy/scripts/fit_all.sh
@@ -1,25 +1,16 @@
-#!/bin/sh
+#!/bin/bash
 
 # Bash script that loops over all detectors and energy channels and calls general_script
 # Pass number of cores as argument
 
-declare -a dets=("n0"
-		"n1"
-		"n2"
-		"n3"
-		"n4"
-		"n5"
-		"n6"
-		"n7"
-		"n8"
-		"n9"
-		"na"
-		"nb")
+declare -a dets=("n0" "n1" "n2" "n3" "n4" "n5" "n6" "n7" "n8" "n9" "na" "nb")
 
 for det in "${dets[@]}"
 do
-    for echan in 0 1 2 3 4 5 6 7
-    do
-	bash nice -19 mpiexec -n $1 python general_script.py  -d $det -e $echan
+    for echan in $(seq 0 7);
+    do echo
+        echo "bash -c 'nice -19 mpiexec -n $1 python general_script.py -d $det -e $echan'"
+        bash -c "nice -19 mpiexec -n $1 python general_script.py -d $det -e $echan"
     done
 done
+
