f0c84a8691f343a55f06fe3c64f4ad3a8d267e0b
Felix Kunzweiler <felix.kunzweiler@tum.de>
2019-10-30 09:59:01 +0100

    Make individual sources selectable

--- gbmbkgpy/scripts/plt_results.py
+++ gbmbkgpy/scripts/plt_results.py
@@ -10,18 +10,19 @@ if __name__ == '__main__':
     parser.add_argument('-c', type=str, help='Path of the config file')
     args = parser.parse_args()
 
-    plot_dict = {}
-    color_dict = {}
+    plot_config = {}
+    component_config = {}
+    style_config = {}
     config_dir_path = os.path.dirname(args.c)
     sys.path.append(config_dir_path)
     module = __import__(args.c, globals(), locals(), ['*'])
     for k in dir(module):
         locals()[k] = getattr(module, k)
 
-    if plot_dict == {} or color_dict == {}:
+    if plot_config == {} or component_config == {} or style_config == {}:
         raise Exception('You should provide a plot_dict and color_dict in your config file')
 
-    result_plot_generator = ResultPlotGenerator(plot_dict, color_dict)
+    result_plot_generator = ResultPlotGenerator(plot_config, component_config, style_config)
 
     # result_plot_generator.add_grb_trigger(grb_name, trigger_time, time_format='UTC', time_offset=0, color='b')
     # result_plot_generator.add_occ_region(occ_name, time_start, time_stop, time_format='UTC', color='grey')
