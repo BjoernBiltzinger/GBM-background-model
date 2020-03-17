3db8750f5854bb4e6267f27348861a54c0c217fe
Felix Kunzweiler <felix.kunzweiler@tum.de>
2020-03-17 20:11:38 +0100

    Add all steps for end to end testing

--- gbmbkgpy/test/test_e2e.py
+++ gbmbkgpy/test/test_e2e.py
@@ -1,7 +1,11 @@
 import os
 import shutil
+import numpy as np
 
+from gbmbkgpy.io.export import DataExporter
+from gbmbkgpy.io.file_utils import file_existing_and_readable
 from gbmbkgpy.io.package_data import get_path_of_external_data_dir
+from gbmbkgpy.io.plotting.plot import Plotter
 from gbmbkgpy.utils.model_generator import BackgroundModelGenerator
 from gbmbkgpy.minimizer.multinest_minimizer import MultiNestFit
 from gbmbkgpy.data.continuous_data import Data
@@ -14,23 +18,7 @@ from gbmbkgpy.utils.response_precalculation import Response_Precalculation
 import pytest
 
 
-# @pytest.fixture
-# def model_generator():
-#     path_of_tests = os.path.dirname(os.path.abspath(__file__))
-#
-#     data_path = get_path_of_external_data_dir()
-#     file_path = os.path.join(data_path, 'ctime', '150126')
-#     file_name = 'glg_ctime_n0_150126_test.pha'
-#
-#     shutil.copy(os.path.join(path_of_tests, file_name), os.path.join(file_path, file_name))
-#
-#     model_gen = BackgroundModelGenerator()
-#
-#     model_gen.from_config_file(os.path.join(path_of_tests, 'config_test.yml'))
-#
-#     return model_gen
-
-
+@pytest.mark.run(order=1)
 def test_model_builder_properties():
     print('Started Model Builder')
     path_of_tests = os.path.dirname(os.path.abspath(__file__))
@@ -56,11 +44,13 @@ def test_model_builder_properties():
     assert isinstance(model_generator.parameter_bounds, dict)
     assert isinstance(model_generator.config, dict)
 
-    pytest.model_generator = model_generator
+    pytest._test_model_generator = model_generator
 
 
+
+@pytest.mark.run(order=2)
 def test_fitting():
-    model_generator = pytest.model_generator
+    model_generator = pytest._test_model_generator
 
     if model_generator.config['fit']['method'] == 'multinest':
         minimizer = MultiNestFit(
@@ -78,3 +68,83 @@ def test_fitting():
         minimizer.plot_marginals()
     else:
         raise KeyError('Invalid fit method')
+
+    true_values = np.array([5.03012970e+01, 8.78085840e-01, 1.10441586e+03, 1.56685886e-02, 4.15783951e-02, 8.69100647e-03, 7.81257334e-02])
+    assert np.allclose(minimizer.best_fit_values, true_values, rtol=1e2)
+
+    minimizer.comp_covariance_matrix()
+
+    assert isinstance(minimizer.cov_matrix, np.ndarray)
+
+    pytest._test_output_dir = minimizer.output_dir
+    pytest._test_best_fit_values = minimizer.best_fit_values
+    pytest._test_cov_matrix = minimizer.cov_matrix
+
+
+
+@pytest.mark.run(order=3)
+def test_data_export():
+    config = pytest._test_model_generator.config
+    data_exporter = DataExporter(
+        data=pytest._test_model_generator.data,
+        model=pytest._test_model_generator.model,
+        saa_object=pytest._test_model_generator.saa_calc,
+        echan_list=config['general']['echan_list'],
+        best_fit_values=pytest._test_best_fit_values,
+        covariance_matrix=pytest._test_cov_matrix
+    )
+
+    result_file_name = "fit_result_{}_{}_e{}.hdf5".format(config['general']['dates'],
+                                                          config['general']['detector'],
+                                                          config['general']['echan_list'])
+
+    data_exporter.save_data(
+        file_path=os.path.join(pytest._test_output_dir, result_file_name),
+        result_dir=pytest._test_output_dir,
+        save_ppc=config['export']['save_ppc']
+    )
+    assert file_existing_and_readable(os.path.join(pytest._test_output_dir, result_file_name))
+
+
+@pytest.mark.run(order=4)
+def test_plotting():
+    config = pytest._test_model_generator.config
+    # Create Plotter object that creates the plots
+    plotter = Plotter(
+        data=pytest._test_model_generator.data,
+        model=pytest._test_model_generator.model,
+        saa_object=pytest._test_model_generator.saa_calc,
+        echan_list=pytest._test_model_generator.config['general']['echan_list']
+    )
+
+    # Create one plot for every echan and save it
+    for index, echan in enumerate(pytest._test_model_generator.config['general']['echan_list']):
+        residual_plot = plotter.display_model(
+            index=index,
+            min_bin_width=config['plot']['bin_width'],
+            show_residuals=config['plot']['show_residuals'],
+            show_data=config['plot']['show_data'],
+            plot_sources=config['plot']['plot_sources'],
+            show_grb_trigger=config['plot']['show_grb_trigger'],
+            change_time=config['plot']['change_time'],
+            ppc=config['plot']['ppc'],
+            result_dir=pytest._test_output_dir,
+            xlim=config['plot']['xlim'],
+            ylim=config['plot']['ylim'],
+            legend_outside=config['plot']['legend_outside']
+        )
+
+    plot_file_name = 'residual_plot_{}_det_{}_echan_{}_bin_width_{}.pdf'.format(config['general']['dates'],
+                                                                                config['general']['detector'],
+                                                                                echan,
+                                                                                config['plot']['bin_width'])
+    residual_plot.savefig(os.path.join(pytest._test_output_dir, plot_file_name), dpi=300)
+
+    assert file_existing_and_readable(os.path.join(pytest._test_output_dir, plot_file_name))
+
+
+
+@pytest.mark.run(order=5)
+def test_delete_outputs():
+    if os.access(pytest._test_output_dir, os.F_OK):
+        shutil.rmtree(pytest._test_output_dir)
