710842c6ba27b2af23b8efdf669bd5fdabf3832b
Felix Kunzweiler <felix.kunzweiler@tum.de>
2020-04-14 19:00:34 +0200

    Move to new gbmgeometry interface

--- gbmbkgpy/test/test_plot_result.py
+++ gbmbkgpy/test/test_plot_result.py
@@ -10,11 +10,11 @@ def test_plot_result():
     print('Started Model Builder')
     path_of_tests = os.path.dirname(os.path.abspath(__file__))
 
-    result_plot_generator = ResultPlotGenerator(
+    result_plot_generator = ResultPlotGenerator.from_result_file(
         config_file=os.path.join(path_of_tests, 'config_plot_test.yml'),
-        data_path=os.path.join(path_of_tests, 'datasets', 'data_of_fit_test.hdf5'))
+        result_data_file=os.path.join(path_of_tests, 'datasets', 'data_of_fit_test.hdf5'))
 
-    result_plot_generator.create_plots()
+    result_plot_generator.create_plots(output_dir=path_of_tests)
 
     for plot_path in result_plot_generator._plot_path_list:
         assert file_existing_and_readable(plot_path)
