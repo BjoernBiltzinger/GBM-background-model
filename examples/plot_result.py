f06b43e43b00203b8f43fef7a8ae32f3d31a05ac
Felix Kunzweiler <felix.kunzweiler@tum.de>
2020-04-28 22:33:18 +0200

    blacken

--- examples/plot_result.py
+++ examples/plot_result.py
@@ -2,12 +2,18 @@ import argparse
 import matplotlib
 from gbmbkgpy.io.plotting.plot_result import ResultPlotGenerator
 
-matplotlib.use('AGG')
+matplotlib.use("AGG")
 
-if __name__ == '__main__':
-    parser = argparse.ArgumentParser(formatter_class=argparse.ArgumentDefaultsHelpFormatter)
-    parser.add_argument('-c', '--config', type=str, help='Path of the config file', required=True)
-    parser.add_argument('-d', '--data_path', type=str, help='Path of the data file', required=True)
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser(
+        formatter_class=argparse.ArgumentDefaultsHelpFormatter
+    )
+    parser.add_argument(
+        "-c", "--config", type=str, help="Path of the config file", required=True
+    )
+    parser.add_argument(
+        "-d", "--data_path", type=str, help="Path of the data file", required=True
+    )
     args = parser.parse_args()
 
     result_plot_generator = ResultPlotGenerator(args.config, args.data_path)
