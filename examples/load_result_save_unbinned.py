dbfea9dea2019acbdbfb91b49c2334820f76c490
Felix Kunzweiler <felix.kunzweiler@tum.de>
2020-04-28 22:33:18 +0200

    blacken

--- examples/load_result_save_unbinned.py
+++ examples/load_result_save_unbinned.py
@@ -31,45 +31,44 @@ def print_progress(text):
 import argparse
 
 parser = argparse.ArgumentParser(formatter_class=argparse.ArgumentDefaultsHelpFormatter)
-parser.add_argument('-config', type=str, help='Path of the config file', required=True)
-parser.add_argument('-result', type=str, help='Path of the result file', required=True)
+parser.add_argument("-config", type=str, help="Path of the config file", required=True)
+parser.add_argument("-result", type=str, help="Path of the result file", required=True)
 args = parser.parse_args()
 
 # Load the config.yml
 with open(args.config) as f:
     config = yaml.load(f)
 
-config['general']['min_bin_width'] = 1E-99
+config["general"]["min_bin_width"] = 1e-99
 
 
 ##### Instantiate background model from result and config file ####
-fit_importer = FitImporter(
-    config=config,
-    fit_result_hdf5=args.result
-)
+fit_importer = FitImporter(config=config, fit_result_hdf5=args.result)
 
 
 ################# Data Export ######################################
 data_exporter = DataExporter(
-    data=              fit_importer.data,
-    model=             fit_importer.model,
-    saa_object=        fit_importer.saa_calc,
-    echan_list=        config['general']['echan_list'],
-    best_fit_values=   fit_importer.best_fit_values,
-    covariance_matrix= None
+    data=fit_importer.data,
+    model=fit_importer.model,
+    saa_object=fit_importer.saa_calc,
+    echan_list=config["general"]["echan_list"],
+    best_fit_values=fit_importer.best_fit_values,
+    covariance_matrix=None,
 )
 
-result_file_name = "fit_result_{}_{}_e{}_unbinned.hdf5".format(config['general']['dates'],
-                                                               config['general']['detector'],
-                                                               config['general']['echan_list'])
+result_file_name = "fit_result_{}_{}_e{}_unbinned.hdf5".format(
+    config["general"]["dates"],
+    config["general"]["detector"],
+    config["general"]["echan_list"],
+)
 output_dir = os.path.dirname(args.result)
 
 data_exporter.save_data(
     file_path=os.path.join(output_dir, result_file_name),
     result_dir=output_dir,
-    save_ppc=config['export']['save_ppc']
+    save_ppc=config["export"]["save_ppc"],
 )
 
-print_progress('Done')
+print_progress("Done")
 # Print the duration of the script
-print('Whole calculation took: {}'.format(datetime.now() - start))
+print("Whole calculation took: {}".format(datetime.now() - start))
