85b5dd3c7c09a6c7fdd128f302cb632a1c241c7e
Felix Kunzweiler <felix.kunzweiler@tum.de>
2020-06-06 10:55:19 +0200

    Fix plot generator

--- examples/plot_result.py
+++ examples/plot_result.py
@@ -14,8 +14,11 @@ if __name__ == "__main__":
     parser.add_argument(
         "-d", "--data_path", type=str, help="Path of the data file", required=True
     )
+    parser.add_argument(
+        "-o", "--output_dir", type=str, help="Output directory", required=True
+    )
     args = parser.parse_args()
 
-    result_plot_generator = ResultPlotGenerator(args.config, args.data_path)
+    result_plot_generator = ResultPlotGenerator.from_result_file(args.config, args.data_path)
 
-    result_plot_generator.create_plots()
+    result_plot_generator.create_plots(args.output_dir)
