c2ebce956b88668447a61dfc57ac23590c6cc588
Felix Kunzweiler <felix.kunzweiler@tum.de>
2020-06-21 13:34:32 +0200

    Fix removal of temp pointsource file

--- gbmbkgpy/utils/model_generator.py
+++ gbmbkgpy/utils/model_generator.py
@@ -332,7 +332,7 @@ class BackgroundModelGenerator(object):
         for i, ps in enumerate(config["setup"]["ps_list"]):
             if ps == "auto_swift":
                 filepath = os.path.join(
-                    get_path_of_external_data_dir(), "ps_auto_swift.dat"
+                    get_path_of_external_data_dir(), ".tmp", "ps_auto_swift.dat"
                 )
                 # Read it as pandas
                 ps_df_add = pd.read_table(filepath, names=["name", "ra", "dec"])
@@ -347,7 +347,6 @@ class BackgroundModelGenerator(object):
                                 "ps_fixed_bound"
                             ]["pl"]["norm"],
                         }
-                os.remove(filepath)
             else:
                 if config["setup"]["ps_list"][ps]["fixed"]:
                     if ps[:4] != "list":
