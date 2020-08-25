7c96a5eddf69430a9fda40a93d003b581e0e7597
Björn Biltzinger <bjoernbiltzinger@gmx.de>
2020-08-25 13:25:01 +0200

    Stan model builder works now

--- gbmbkgpy/utils/stan.py
+++ gbmbkgpy/utils/stan.py
@@ -3,6 +3,404 @@ from scipy.interpolate import interp1d
 import arviz as av
 import matplotlib.pyplot as plt
 
+class StanModelConstructor(object):
+    """
+    Object to construct the .stan model
+    """
+
+    def __init__(self, model_generator):
+
+        data = model_generator.data
+        model = model_generator.model
+
+        num_dets = len(data.detectors)
+        num_echans = len(data.echans)
+
+        # How many of which sources?
+
+        # Global
+        sources = model.global_sources
+        self._num_fixed_global_sources = len(sources)
+        if self._num_fixed_global_sources>0:
+            self._use_fixed_global_sources = True
+        else:
+            self._use_fixed_global_sources = False
+
+        # Cont
+        sources = model.continuum_sources
+        self._num_cont_sources = int(len(sources)/num_echans)
+        if self._num_cont_sources>0:
+            assert self._num_cont_sources==2, "Must be two cont sources!"
+            self._use_cont_sources = True
+        else:
+            self._use_cont_sources = False
+
+        # SAA
+        sources = model.saa_sources
+        self._num_saa_exits = int(len(sources)/num_echans)
+        if self._num_saa_exits>0:
+            self._use_saa = True
+        else:
+            self._use_saa = False
+
+        # Free spectrum
+        sources = model.fit_spectrum_sources
+        self._num_free_ps = 0
+        self._use_free_earth = False
+        self._use_free_cgb = False
+        for key in sources.keys():
+            if k == "Earth occultation":
+                self._use_free_earth = True
+            if k == "CGB":
+                self._use_free_cgb = True
+            else:
+                self._num_free_ps += 1
+
+        if self._num_free_ps > 0:
+            self._use_free_ps = True
+        else:
+            self._use_free_ps = False
+
+    def create_stan_file(self, save_path):
+        text = self.function_block() + self.data_block() + self.trans_data_block() + self.parameter_block() + self.trans_parameter_block() + self.model_block() + self.generated_block()
+
+        with open(save_path, "w") as f:
+            f.write(text)
+
+
+    def function_block(self):
+        text = "functions { \n"
+
+        if self._use_saa:
+            text += "\tvector saa_total(vector[] saa_norm_vec, vector[] saa_decay_vec, matrix[] t_t0, int num_data_points,int num_saa_exits){\n"\
+                "\t\tvector[num_data_points] total_saa_counts = rep_vector(0.0, num_data_points);\n"\
+                "\t\tfor (i in 1:num_saa_exits){\n"\
+                "\t\t\ttotal_saa_counts += saa_norm_vec[i]./saa_decay_vec[i].*(exp(-t_t0[i,:,1].*saa_decay_vec[i]) - exp(-t_t0[i,:, 2].*saa_decay_vec[i, :]));\n"\
+                "\t\t}\n"\
+                "\t\treturn total_saa_counts;\n\t}\n\n"
+
+        # Main partial sum function
+        main = "\treal partial_sum(int[] counts, int start, int stop\n"
+
+        if self._use_fixed_global_sources:
+            main += "\t, vector[] base_counts_array, real[] norm_fixed\n"
+
+        if self._use_free_earth:
+            main += "\t, matrix base_response_array_earth, vector earth_spec\n"
+
+        if self._use_free_cgb:
+            main += "\t, matrix base_response_array_cgb, vector cgb_spec\n"
+
+        if self._use_free_ps:
+            main += "\t, matrix base_response_array_free_ps, vector[] ps_spec\n"
+
+        if self._use_cont_sources:
+            main += "\t, vector[] norm_cont_vec, vector[] base_counts_array_cont\n"
+
+        if self._use_saa:
+            main += "\t, matrix[] t_t0, vector[] saa_decay_vec, vector[] saa_norm_vec\n"
+
+        main += "\t){\n\t\treturn poisson_propto_lpmf(counts | 0\n"
+
+        if self._use_saa:
+            for i in range(self._num_saa_exits):
+                main += f"\t\t\t+saa_norm_vec[{i+1}, start:stop]./saa_decay_vec[{i+1}, start:stop].*"\
+                        f"(exp(-t_t0[{i+1},start:stop,1].*saa_decay_vec[{i+1}, start:stop])-"\
+                        f"exp(-t_t0[{i+1},start:stop,2].*saa_decay_vec[{i+1}, start:stop]))\n"
+
+        if self._use_fixed_global_sources:
+            for i in range(self._num_fixed_global_sources):
+                main += f"\t\t\t+norm_fixed[{i+1}]*base_counts_array[{i+1},start:stop]\n"
+
+        if self._use_cont_sources:
+            for i in range(self._num_cont_sources):
+                main += f"\t\t\t+norm_cont_vec[{i+1}][start:stop].*base_counts_array_cont[{i+1}][start:stop]\n"
+
+        if self._use_free_earth:
+            main += "\t\t\t+base_response_array_earth[start:stop]*earth_spec\n"
+
+        if self._use_free_cgb:
+            main += "\t\t\t+base_response_array_cgb[start:stop]*cgb_spec\n"
+
+        if self._use_free_ps:
+            for i in range(self._num_free_ps):
+                main += f"\t\t\t+base_response_array_ps[{i+1},start:stop]*ps_spec[{i+1}]\n"
+
+        main += "\t\t\t);\n\t}\n"
+
+        text = text + main + "}\n\n"
+        return text
+
+    def data_block(self):
+        # Start
+        text = "data { \n"
+        # This we need always:
+        text = text + "\tint<lower=1> num_time_bins;\n"
+        text = text + "\tint<lower=1> num_dets;\n"
+        text = text + "\tint<lower=1> num_echans;\n"
+
+        text = text + "\tint<lower=1> rsp_num_Ein;\n"
+        text = text + "\tvector[rsp_num_Ein] Ebins_in[2];\n"
+
+        text = text + "\tint<lower=1> grainsize;\n"
+        text = text + "\tmatrix[num_time_bins, 2] time_bins;\n"
+
+        text = text + "\tint counts[num_time_bins*num_dets*num_echans];\n"
+
+        # Optional input
+        if self._use_fixed_global_sources:
+            text = text + "\tint<lower=0> num_fixed_comp;\n"
+            text = text + "\tvector[num_time_bins*num_dets*num_echans] base_counts_array[num_fixed_comp];\n"
+
+        if self._use_cont_sources:
+            text = text + "\tint num_cont_comp;\n"
+            text = text + "\tvector[num_time_bins*num_dets*num_echans] base_counts_array_cont[num_cont_comp];"
+
+        if self._use_saa:
+            text = text + "\tint num_saa_exits;\n"
+            text = text + "\tvector[num_saa_exits] saa_start_times;\n"
+
+        # Close
+        text = text + "}\n\n"
+        return text
+
+    def trans_data_block(self):
+        text = "transformed data { \n"
+
+        text += "\tint num_data_points = num_time_bins*num_dets*num_echans;\n"
+
+        if self._use_saa:
+            text += "\tmatrix[num_data_points,2] t_t0[num_saa_exits];\n"
+
+        text += "\tfor (j in 1:num_saa_exits){\n"\
+            "\t\tfor (i in 1:num_time_bins){\n"\
+            "\t\t\tif (time_bins[i,1]>saa_start_times[j]){\n"\
+            "\t\t\t\tt_t0[j,(i-1)*num_dets*num_echans+1:i*num_dets*num_echans] = rep_matrix(time_bins[i]-saa_start_times[j], num_dets*num_echans);\n"\
+            "\t\t\t}\n"\
+            "\t\t\telse {\n"\
+            "\t\t\t\tt_t0[j,(i-1)*num_dets*num_echans+1:i*num_dets*num_echans] = rep_matrix(0.0, num_dets*num_echans, 2);\n"\
+            "\t\t\t}\n\t\t}\n\t}\n"
+
+        text = text + "}\n\n"
+        return text
+
+
+    def parameter_block(self):
+        text = "parameters { \n"
+
+        if self._use_fixed_global_sources:
+            text += "\treal log_norm_fixed[num_fixed_comp];\n"
+
+        if self._use_saa:
+            text += "\treal log_norm_saa[num_saa_exits,num_dets, num_echans];\n"
+            text += "\treal<lower=0.01,upper=10> decay_saa[num_saa_exits,num_dets, num_echans];\n"
+
+        if self._use_cont_sources:
+            text += "\treal log_norm_cont[num_cont_comp, num_dets, num_echans];\n"
+
+        if self._use_free_earth:
+            text += "\treal log_norm_earth;\n"
+            text += "\treal alpha_earth;\n"
+            text += "\treal Ec_earth;\n"
+
+        if self._use_free_cgb:
+            text += "\treal log_norm_cgb;\n"
+            text += "\tordered[2] indices_cgb;\n"
+            text += "\treal Eb_cgb;\n"
+
+        if self._use_free_ps:
+            text += "\treal log_norm_free_ps[num_free_ps_comp];\n"
+            text += "\treal index_free_ps[num_free_ps_comp];\n"
+
+        text = text + "}\n\n"
+        return text
+
+    def trans_parameter_block(self):
+        text = "transformed parameters { \n"
+
+        if self._use_fixed_global_sources:
+            text += "\treal norm_fixed[num_fixed_comp] = exp(log_norm_fixed);\n"
+
+        if self._use_saa:
+            text += "\treal norm_saa[num_saa_exits,num_dets, num_echans]=exp(log_norm_saa);\n"
+
+            text += "\tvector[num_data_points] saa_decay_vec[num_saa_exits];\n"
+            text += "\tvector[num_data_points] saa_norm_vec[num_saa_exits];\n"
+
+        if self._use_cont_sources:
+            text += "\treal norm_cont[num_cont_comp, num_dets, num_echans] = exp(log_norm_cont);\n"
+            text += "\tvector[num_data_points] norm_cont_vec[num_cont_comp];\n"
+
+        if self._use_free_earth:
+            text += "\treal norm_earth = exp(log_norm_earth);\n"
+            text += "\tvector[rsp_num_Ein] earth_spec;\n"
+
+        if self._use_free_cgb:
+            text += "\treal norm_cgb = exp(log_norm_cgb);\n"
+            text += "\tvector[rsp_num_Ein] cgb_spec;\n"
+
+        if self._use_free_ps:
+            text += "\treal norm_free_ps[num_free_ps_comp = exp(log_norm_free_ps);\n"
+            text += "\tvector[rsp_num_Ein] ps_spec[num_free_ps_comp];\n"
+
+        if self._use_cont_sources:
+            text += "\tfor (l in 1:num_cont_comp){\n"\
+                "\t\tfor (i in 1:num_dets){\n"\
+                "\t\t\tfor (j in 1:num_echans){\n"\
+                "\t\t\t\tfor (k in 1:num_time_bins){\n"\
+                "\t\t\t\t\tnorm_cont_vec[l][(k-1)*(num_dets*num_echans)+(i-1)*num_echans+j] = norm_cont[l,i,j];\n"\
+                "\t\t\t\t}\n\t\t\t}\n\t\t}\n\t}\n"
+
+        if self._use_saa:
+            text += "\tfor (l in 1:num_saa_exits){\n"\
+                "\t\tfor (i in 1:num_dets){\n"\
+                "\t\t\tfor (j in 1:num_echans){\n"\
+                "\t\t\t\tfor (k in 1:num_time_bins){\n"\
+                "\t\t\t\t\tsaa_decay_vec[l][(k-1)*(num_dets*num_echans)+(i-1)*num_echans+j] = 0.0001*decay_saa[l,i,j];\n"\
+                "\t\t\t\t\tsaa_norm_vec[l][(k-1)*(num_dets*num_echans)+(i-1)*num_echans+j] = norm_saa[l,i,j];\n"\
+                "\t\t\t\t}\n\t\t\t}\n\t\t}\n\t}\n"
+
+        if self._use_free_ps:
+            text += "\tfor (j in 1:num_free_ps_comp){\n"\
+                "\t\tfor (i in 1:rsp_num_Ein){\n"\
+                "\t\t\tps_spec[j][i] = (Ebins_in[2,i]-Ebins_in[1,i])*norm_free_ps[j]*(pow(0.1*Ebins_in[1,i], -index_free_ps[j])+pow(0.1*Ebins_in[2,i], -index_free_ps[j]))/2;\n"\
+                "\t\t}\n\t}\n"
+
+        if self._use_free_earth:
+            text += "\tfor (i in 1:rsp_num_Ein){\n"\
+                "\t\tearth_spec[i] = (Ebins_in[2,i]-Ebins_in[1,i])*0.5*norm_earth*(pow(Ebins_in[1,i], -alpha_earth)*exp(-Ec_earth/Ebins_in[1,i])+pow(Ebins_in[2,i], -alpha_earth)*exp(-Ec_earth/Ebins_in[2,i]));\n"\
+                "\t}\n"
+
+        if self._use_free_cgb:
+            text += "\tfor (i in 1:rsp_num_Ein){\n"\
+                "\t\tcgb_spec[i] = (Ebins_in[2,i]-Ebins_in[1,i])*0.5*(norm_cgb/(pow(Ebins_in[1,i]/Eb_cgb, indices_cgb[1])+pow(Ebins_in[1,i]/Eb_cgb, indices_cgb[2]))+norm_cgb/(pow(Ebins_in[2,i]/Eb_cgb, indices_cgb[1])+pow(Ebins_in[2,i]/Eb_cgb, indices_cgb[2])));\n"\
+                "\t}\n"
+
+        text = text + "}\n\n"
+        return text
+
+    def model_block(self):
+        text = "model { \n"
+
+        # Priors - Fixed at the moment!:
+        # TODO Use config file to get the priors!
+        if self._use_fixed_global_sources:
+            text += "\tnorm_fixed ~ lognormal(0, 1);\n"
+
+        if self._use_free_earth:
+            text += "\talpha_earth ~ normal(-5, 2);\n"
+            text += "\tEc_earth ~ normal(30,4);\n"
+            text += "\tlog_norm_earth ~ normal(-4.1,0.5);\n"
+
+        if self._use_free_cgb:
+            text += "\talpha_cgb ~ normal(1.32, 0.2);\n"
+            text += "\tbeta_cgb ~ normal(2.88, 0.3);\n"
+            text += "\tEb_cgb ~ normal(35,5);\n"
+            text += "\tlog_norm_cgb ~ normal(-2.3,0.5);\n"
+
+        if self._use_free_ps:
+            text += "\tindex_free_ps ~ normal(3,1);\n"
+            text += "\tlog_norm_free_ps ~ normal(0,1);\n"
+
+        if self._use_cont_sources:
+            text += "\tfor (d in 1:num_dets){\n"\
+                "\t\tfor (g in 1:num_echans){\n"\
+                "\t\t\tlog_norm_cont[:,d,g] ~ normal(0,1);\n"\
+                "\t\t}\n\t}\n"
+
+        if self._use_saa:
+            text += "\tfor (d in 1:num_dets){\n"\
+                "\t\tfor (e in 1:num_echans){\n"\
+                "\t\t\tlog_norm_saa[:,d,e] ~ normal(0,1);\n"\
+                "\t\t\tdecay_saa[:,d,e] ~ lognormal(0,1);\n"\
+                "\t\t}\n\t}\n"
+
+        # Reduce sum call
+        main = "\ttarget += reduce_sum(partial_sum, counts, grainsize\n"
+        if self._use_fixed_global_sources:
+            main += "\t\t, base_counts_array, norm_fixed\n"
+
+        if self._use_free_earth:
+            main += "\t\t, base_response_array_earth, earth_spec\n"
+
+        if self._use_free_cgb:
+            main += "\t\t, base_response_array_cgb, cgb_spec\n"
+
+        if self._use_free_ps:
+            main += "\t\t, base_response_array_free_ps, ps_spec\n"
+
+        if self._use_cont_sources:
+            main += "\t\t, norm_cont_vec,  base_counts_array_cont\n"
+
+        if self._use_saa:
+            main += "\t\t, t_t0, saa_decay_vec, saa_norm_vec\n"
+
+        main += "\t);\n"
+
+        text = text + main + "}\n\n"
+        return text
+
+    def generated_block(self):
+        text = "generated quantities { \n"
+
+        text += "\tint ppc[num_data_points];\n"
+        text += "\tvector[num_data_points] tot;\n"
+
+        if self._use_saa:
+            text += "\tvector[num_data_points] f_saa;\n"
+
+        if self._use_cont_sources:
+            text += "\tvector[num_data_points] f_cont[num_cont_comp];\n"
+
+        if self._use_fixed_global_sources:
+            text += "\tvector[num_data_points] f_fixed_global[num_fixed_comp];\n"
+
+        if self._use_free_earth:
+            text += "\tvector[num_data_points] f_earth;\n"
+
+        if self._use_free_cgb:
+            text += "\tvector[num_data_points] f_cgb;\n"
+
+        if self._use_free_ps:
+            text += "\tvector[num_data_points] f_free_ps[num_free_ps_comp];\n"
+
+
+
+
+        if self._use_saa:
+            text += "\tf_saa = saa_total(saa_norm_vec, saa_decay_vec, t_t0, num_data_points, num_saa_exits);\n"
+            text += "\ttot += f_saa;\n"
+
+        if self._use_cont_sources:
+            text += "\tfor (i in 1:num_cont_comp){\n"\
+                "\t\tf_cont[i] = norm_cont_vec[i].*base_counts_array_cont[i];\n"\
+                "\t\ttot += f_cont[i];\n"\
+                "\t}\n"
+
+        if self._use_fixed_global_sources:
+            text += "\tfor (i in 1:num_fixed_comp){\n"\
+                "\t\tf_fixed_global[i]=norm_fixed[i]*base_counts_array[i];\n"\
+                "\t\ttot+=f_fixed_global[i];\n"\
+                "\t}\n"
+
+        if self._use_free_earth:
+            text += "\tf_earth = base_response_array_earth*earth_spec;\n"
+            text += "\ttot += f_earth;\n"
+
+        if self._use_free_cgb:
+            text += "\tf_cgb = base_response_array_cgb*cgb_spec;\n"
+            text += "\ttot += f_cgb;\n"
+
+        if self._use_free_ps:
+            text += "\tfor (i in 1:num_free_ps_comp){\n"\
+                "\t\tf_free_ps[i]=base_response_array_free_ps[i]*ps_spec[i];\n"\
+                "\t\ttot+=f_free_ps[i];\n"\
+                "\t}\n"
+
+
+        text = text + "}\n\n"
+        return text
+
 
 class StanDataConstructor(object):
     """
