f10af27ebdfad5903a56d54626db015a0215c432
BjoernBiltzinger <bjoernbiltzinger@gmx.de>
2022-10-07 11:45:14 +0200

    clean downloading

--- gbmbkgpy/io/downloading.py
+++ gbmbkgpy/io/downloading.py
@@ -1,10 +1,13 @@
 import os
 import shutil
-from gbmbkgpy.io.file_utils import file_existing_and_readable
-
-from gbmbkgpy.io.package_data import get_path_of_data_dir, get_path_of_external_data_dir
+from urllib.error import HTTPError
 from astropy.utils.data import download_file
 
+from gbmbkgpy.io.file_utils import file_existing_and_readable
+from gbmbkgpy.utils.mpi import check_mpi
+from gbmbkgpy.io.package_data import get_path_of_data_dir, get_path_of_external_data_dir
+
+using_mpi, rank, size, comm = check_mpi()
 
 def download_flares(year):
     """This function downloads a yearly solar flar data file and stores it in the appropriate folder\n
@@ -73,82 +76,57 @@ def download_lat_spacecraft(week):
     shutil.move(path_to_file, file_path + file_name)
 
 
-def download_data_file(date, type, detector="all"):
+def download_gbm_file(date, data_type, detector="all"):
     """
     Download CTIME / CSPEC or POSHIST files
 
     :param date: string like '180407'
-    :param type: string like 'ctime', 'cspec', 'poshist'
+    :param data_type: string like 'ctime', 'cspec', 'poshist'
     :param detector: string like 'n1', 'n2' or 'all' for poshist
     :return:
     """
 
-    year = "20%s" % date[:2]
-    month = date[2:-2]
-    day = date[-2:]
+    if rank == 0:
+        assert data_type in ['ctime', 'cspec', 'poshist'], "Wrong data_type..."
 
-    data_path = get_path_of_external_data_dir()
-    if not os.access(data_path, os.F_OK):
-        print("Making New Directory")
-        os.mkdir(data_path)
+        year = "20%s" % date[:2]
+        month = date[2:-2]
+        day = date[-2:]
 
-    # poshist files are not stored in a sub folder of the date
-    if type == "poshist":
-        file_path = os.path.join(data_path, type)
-        file_type = "fit"
-    else:
-        folder_path = os.path.join(data_path, type)
-        if not os.access(folder_path, os.F_OK):
-            print("Making New Directory")
-            os.mkdir(folder_path)
+        data_path = get_path_of_external_data_dir()
 
-        file_path = os.path.join(data_path, type, date)
-        file_type = "pha"
+        file_path = data_path / data_type / date
+        file_path.mkdir(parents=True, exist_ok=True)
+        # poshist files are not stored in a sub folder of the date
+        if data_type == "poshist":
+            file_type = "fit"
+        else:
+            file_type = "pha"
 
-    if not os.access(file_path, os.F_OK):
-        print("Making New Directory")
-        os.mkdir(file_path)
+        final_path = (file_path /
+                      f"glg_{data_type}_{detector}_{date}_v00.{file_type}")
 
-    try:
-        url = "https://heasarc.gsfc.nasa.gov/FTP/fermi/data/gbm/daily/{0}/{1}/{2}/current/glg_{3}_{4}_{5}_v00.{6}".format(
-            year, month, day, type, detector, date, file_type
-        )
+        if not final_path.exists():
+            base_url = (f"https://heasarc.gsfc.nasa.gov/FTP/fermi/data/"
+                        f"gbm/daily/{year}/{month}/{day}/current/"
+                        f"glg_{data_type}_{detector}_{date}_v0")
 
-        path_to_file = download_file(url)
-    except:
-        try:
-            url = "https://heasarc.gsfc.nasa.gov/FTP/fermi/data/gbm/daily/{0}/{1}/{2}/current/glg_{3}_{4}_{5}_v01.{6}".format(
-                year, month, day, type, detector, date, file_type
-            )
-
-            path_to_file = download_file(url)
-        except:
-            try:
-                url = "https://heasarc.gsfc.nasa.gov/FTP/fermi/data/gbm/daily/{0}/{1}/{2}/current/glg_{3}_{4}_{5}_v02.{6}".format(
-                    year, month, day, type, detector, date, file_type
-                )
-
-                path_to_file = download_file(url)
-            except:
+            path_to_file = None
+            for version in ["0", "1", "2", "3", "4"]:
                 try:
-                    url = "https://heasarc.gsfc.nasa.gov/FTP/fermi/data/gbm/daily/{0}/{1}/{2}/current/glg_{3}_{4}_{5}_v03.{6}".format(
-                        year, month, day, type, detector, date, file_type
-                    )
+                    path_to_file = download_file(f"{base_url}{version}.{file_type}")
+                except HTTPError:
+                    pass
+                if path_to_file is not None:
+                    break
 
-                    path_to_file = download_file(url)
-                except:
-                    try:
-                        url = "https://heasarc.gsfc.nasa.gov/FTP/fermi/data/gbm/daily/{0}/{1}/{2}/current/glg_{3}_{4}_{5}_v04.{6}".format(
-                            year, month, day, type, detector, date, file_type
-                        )
+            if path_to_file is None:
+                print(f"No version found for the url {base_url}?.{file_type}")
 
-                        path_to_file = download_file(url)
-                    except:
-                        print("This url not found {}".format(url))
+            shutil.move(path_to_file, final_path)
 
-    file_name = "glg_{0}_{1}_{2}_v00.{3}".format(type, detector, date, file_type)
-
-    shutil.move(path_to_file, os.path.join(file_path, file_name))
+    if using_mpi:
+        comm.Barrier()
 
 
 def download_trigdata_file(trigger, type, detector="all"):
@@ -234,7 +212,7 @@ def download_files(data_type, det, day):
     )
 
     if not file_existing_and_readable(datafile_path):
-        download_data_file(day, data_type, det)
+        download_gbm_file(day, data_type, det)
 
     if not file_existing_and_readable(poshistfile_path):
-        download_data_file(day, "poshist")
+        download_gbm_file(day, "poshist")
