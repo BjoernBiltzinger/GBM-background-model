2ec206f798376ec911e2e3c507f2d2a5979a5300
BjoernBiltzinger <bjoernbiltzinger@gmx.de>
2022-10-07 11:45:41 +0200

    change os to pathlib

--- gbmbkgpy/io/package_data.py
+++ gbmbkgpy/io/package_data.py
@@ -1,6 +1,6 @@
 import pkg_resources
 import os
-
+from pathlib import Path
 
 def get_path_of_data_file(source_type, data_file):
 
@@ -8,25 +8,25 @@ def get_path_of_data_file(source_type, data_file):
         "gbmbkgpy", "data/datasets/{0}/{1}".format(source_type, data_file)
     )
 
-    return file_path
+    return Path(file_path)
 
 
 def get_path_of_data_dir():
 
     file_path = pkg_resources.resource_filename("gbmbkgpy", "data/datasets")
 
-    return file_path
+    return Path(file_path)
 
 
 def get_path_of_external_data_dir():
 
     file_path = os.environ["GBMDATA"]
 
-    return file_path
+    return Path(file_path)
 
 
 def get_path_of_external_data_file(source_type, data_file):
 
-    file_path = os.path.join(get_path_of_external_data_dir(), source_type, data_file)
+    file_path = get_path_of_external_data_dir() / source_type / data_file
 
     return file_path
